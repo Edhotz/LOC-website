0376d0e78f8d8f93696b41c9bad2744a018646f8
Edmauro Goma <95288145+EdmauroGoma@users.noreply.github.com>
2022-10-18 09:32:03 +0100

    remoção de imagem e diminuição de fontes

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -59,7 +59,7 @@ export const HeroContent = styled.div`
 
 export const HeroH1 = styled.h1`
     color: #fff;
-    font-size: 48px;
+    font-size: 34px;
     text-align: center;
 
     @media screen and (max-width: 768px) {
@@ -74,7 +74,7 @@ export const HeroH1 = styled.h1`
 export const HeroP = styled.p`
     margin-top: 24px;
     color: #fff;
-    font-size: 24px;
+    font-size: 18px;
     text-align: center;
     max-width: 800px;
 
--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -70,7 +70,7 @@ export const TopLine = styled.p`
 
 export const Heading = styled.h1`
     margin-bottom: 24px;
-    font-size: 48px;
+    font-size: 28px;
     line-height: 1.1;
     font-weight: 600;
     color: ${({ lightText }) => (lightText ? '#f7f8fa' : '#010606')};
@@ -83,8 +83,8 @@ export const Heading = styled.h1`
 export const Subtitle = styled.p`
     max-width: 440px;
     margin-bottom: 35px;
-    font-size: 18px;
-    line-height: 24px;
+    font-size: 16px;
+    line-height: 22px;
     color: ${({ darkText }) => (darkText ? '#010606' : '#fff')};
 `
 
