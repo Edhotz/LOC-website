29aa89e40701b1e7fd05d8551ac7390ec86c5028
EdmauroGoma <edmauroenterprise2021@gmail.com>
2022-11-29 10:02:19 +0100

    refactoring and responsivity

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Video from '../../videos/Video.mp4'
+import Video from '../../videos/Video.webm'
 import { HeroContainer, HeroBg, VideoBg, HeroContent, HeroH1, HeroP, HeroBtnWrapper, ArrowForward, ArrowRight } from './HeroElements'
 import { Button } from '../ButtonElement'
 
