0b15b335e370c4b90a00e2898e89322fcc43a408
EdmauroGoma <edmauroenterprise2021@gmail.com>
2023-05-23 10:09:37 +0100

    ajuste de cards

--- src/components/InfoSectionMarketing/InfoSectionElements.js
+++ src/components/InfoSectionMarketing/InfoSectionElements.js
@@ -63,7 +63,7 @@ export const MarketingServices = styled.div`
     height: 12.875em;
     padding: 1rem;
     background-color: rgba(255, 255, 255, 0.074);
-    border: 2px solid rgba(000, 000, 255, 10);
+    border: 1px solid rgba(000, 255, 255, 0.090);
     -webkit-backdrop-filter: blur(20px);
     backdrop-filter: blur(20px);
     border-radius: 0.7rem;
