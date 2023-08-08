5961202b2a926734c67fb324348d60b4e68ddf17
Edmauro Goma <edmauro@Edkali.EdKali>
2023-08-08 10:53:52 +0100

    fix: chart config

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Dashboard from "../components/Dashboard";
-import { RequireAuth } from "../AuthProvider/PrivateRoute";
 
 const DashboardPage = () => {
   return (
